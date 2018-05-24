b98ebccbc41b3ddfddf87f6ccc1a37f0d7664813
Irina <irenekochetkova@gmail.com>
2018-05-23 22:35:26 -0700

    added solution of the task Wherefore art thou

---
+++ Wherefore_art_thou.js
@@ -0,0 +1,36 @@
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+// For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+// Here are some helpful links:
+
+//     Global Object
+
+//     Object.prototype.hasOwnProperty()
+
+//     Object.keys()
+// ********************************************************************************************
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+   for (var key in source) {
+      var valueSource = source[key];
+   }
+      for (var i = 0; i < collection.length; i++) {
+        var obj = collection[i];
+          for (var name in obj) {
+            var valueObj = obj[name];
+            if (valueSource === valueObj && key === name) {
+              arr.push(obj);
+            }
+          } 
+      }
+  
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
