7542878c245847785cdfb413f43dc9cbce34b265
Irina <irenekochetkova@gmail.com>
2018-05-25 08:32:32 -0700

    added solution of the task Boo who

--- 08_Missing_letters.js
+++ 08_Missing_letters.js
@@ -1,3 +1,16 @@
+// Find the missing letter in the passed letter range and return it.
+
+// If all letters are present in the range, return undefined.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+// Here are some helpful links:
+
+//     String.prototype.charCodeAt()
+
+//     String.fromCharCode()
+// *****************************************************
+
 function fearNotLetter(str) {
   for (var i = 0; i < str.length - 1; i++) {
     var unicode = str.charCodeAt(i) - str.charCodeAt(i + 1);
@@ -8,4 +21,4 @@ function fearNotLetter(str) {
   }
 }
 
-console.log(fearNotLetter("abcde"));
+console.log(fearNotLetter("abce"));
---
+++ 09_Boo_who.js
@@ -0,0 +1,16 @@
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+// Here are some helpful links:
+
+//     Boolean Objects
+// ****************************************************
+
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
